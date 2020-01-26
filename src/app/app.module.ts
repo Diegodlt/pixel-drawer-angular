b0f8bc27d2b3c5dfb6a4c71f60ecb6c844182840
Diego <diegodltl@gmail.com>
2020-01-26 10:54:44 -0500

    Create pixel component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PixelComponent } from './pixel/pixel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PixelComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pixel/pixel.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input, ChangeDetectionStrategy, HostListener, Output, EventEmitter } from '@angular/core';
+
+@Component({
+    selector: 'pixel',
+    template: `
+        <div></div>
+    `,
+    styles: [`
+        div {
+            border: 1px solid black;
+            display: inline-block;
+            width: 10px;
+            height: 10px;
+            float: left;
+            border-color: rgba( 160, 160, 255, 0.4);
+        }
+    
+    `]
+})
+
+export class PixelComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() { }
+}
